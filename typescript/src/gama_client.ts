f47a4f9c45c3adecde76b7c1e6cad2cd6a0668f0
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-03-18 13:53:32 +0700

    changed the success function to resolve with the message without stringifying it, makes it easier to parse

--- typescript/src/gama_client.ts
+++ typescript/src/gama_client.ts
@@ -271,8 +271,8 @@ export default class GamaClient {
                 const type = message.type
                 if (type === successMessage) {
                     this.gama_socket.removeEventListener('message', onMessage)
-                    resolve(JSON.stringify(message))
-                } else if (GAMA_ERROR_MESSAGES.includes(type)){
+                    resolve(message)
+                } else if (GAMA_ERROR_MESSAGES.includes(type)) {
                     this.gama_socket.removeEventListener('message', onMessage)
                     this.setContentError(message.content)
                     reject(`Couldn't execute command on the Gama Server. ${type}: ${JSON.stringify(message.content)}`)
